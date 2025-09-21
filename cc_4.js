785ca65cb70a4347f2d3a4b92c84fdcf8003985c
Ibrokhim Ushurov <ibrokhimushurov@usf.edu>
2025-09-21 17:29:31 -0400

    Listing products

---
+++ cc_4.js
@@ -0,0 +1,13 @@
+// Coding Challange 4
+
+// Step 2: Product list 
+
+const products = [
+
+    {sku: "A-900", name: "Mini Fridge", category: "electronics", price: 89.99, invnetory: 48 },
+    {sku: "C-900", name: "Shoes", category: "apparel", price: 48.59, invnetory: 32 },
+    {sku: "N-900", name: "Crackers", category: "groceries", price: 7.92, invnetory: 63 },
+    {sku: "L-900", name: "Towels", category: "household", price: 18.99, invnetory: 107 },
+    {sku: "K-900", name: "game CDs", category: "entertainment", price: 39.99, invnetory: 206 },
+];
+
